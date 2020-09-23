472a3ce40e4b4aea4a865d685d730def38cf3599
Empratyush <Empratyush@gmail.com>
2020-09-23 17:15:49 +0530

    added vscodium

--- allpackage.sh
+++ allpackage.sh
@@ -183,6 +183,13 @@ cp -b ./spotify /usr/bin/
 
 echo -e "\n spotify Gui and player is installed \n"
 
+echo -e "\n Installing vscodeium \n"
+wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg | gpg --dearmor | dd of=/etc/apt/trusted.gpg.d/vscodium.gpg 
+echo 'deb https://paulcarroty.gitlab.io/vscodium-deb-rpm-repo/debs/ vscodium main' | tee --append /etc/apt/sources.list.d/vscodium.list 
+
+apt update 
+apt install codium 
+
 
 echo -e "\n installing tor \n"
 
